1f33ecd7ad5e81ab1cbb347f205762fdd229535e
Woobeen906 <67691520+Woobeen906@users.noreply.github.com>
2021-02-01 20:02:49 +0900

    A. Theatre Square

--- 1-A.cpp
+++ 1-A.cpp
@@ -4,11 +4,5 @@ using namespace std;
 int main() {
 	long long n, m, a;
 	cin >> n >> m >> a;
-	long long resultn, resultm;
-	if (n % a > 0)resultn = n / a + 1;
-	else resultn = n / a;
-	if (m % a > 0)resultm = m / a + 1;
-	else resultm = m / a;
-
-	cout << resultm + resultn;
+	cout << ((n + a - 1) / a) * ((m + a - 1) / a);
 }
